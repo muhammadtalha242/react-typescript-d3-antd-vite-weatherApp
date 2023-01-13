eccd381314c9d437abbf17e7a7906c518983e6e4
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-14 02:55:05 +0500

    input search and autocomplete

--- src/components/common/input-search.tsx
+++ src/components/common/input-search.tsx
@@ -1,53 +1,41 @@
 import React, { useState } from "react";
-import { AutoComplete } from "antd";
+import { AutoComplete, Input } from "antd";
 
 interface props {
   value?: string;
-  setValue?: Function;
+  setValue: Function;
   name?: string;
+  displayOptions: ISearch;
+  handleSelectedValue: Function;
 }
-const mockVal = (str: string, repeat = 1) => ({
-  value: str.repeat(repeat),
-});
-const InputSearch = (props: props) => {
-  const [value, setValue] = useState("");
-  const [options, setOptions] = useState<{ value: string }[]>([]);
 
-  const handleInput = (data: string): void => {
+export interface ISearch {
+  searchTerm: string;
+  searchedOptions: { value: string; label: string; coordinates: {lat: string, lon: string} }[];
+}
+
+const InputSearch = (props: props) => {
+  const onSearch = (data: string): void => {
     if (props.setValue)
       props.setValue({
-        name: props.name,
         value: data,
       });
   };
 
-  const onSearch = (searchText: string) => {
-    console.log("SEARCH THIS LOCATION: ", searchText);
-  };
-
-  const onSelect = (data: string) => {
-    //send this value
-    console.log("onSelect", data);
-  };
-
-  const onChange = (data: string) => {
-    console.log("ON change", data);
-    setOptions(
-      !data ? [] : [mockVal(data), mockVal(data, 2), mockVal(data, 3)]
-    );
-    setValue(data);
+  const onSelect = (data: string, selectedOption: any) => {
+    if (props.handleSelectedValue) props.handleSelectedValue(selectedOption);
   };
 
   return (
     <AutoComplete
-      value={value}
-      options={options}
-      style={{ width: 200 }}
+      value={props.displayOptions.searchTerm}
+      options={props.displayOptions.searchedOptions}
+      style={{ width: 300 }}
       onSelect={onSelect}
       onSearch={onSearch}
-      onChange={onChange}
-      placeholder="Search location"
-    />
+    >
+      <Input.Search placeholder="Search location" />
+    </AutoComplete>
   );
 };
 
