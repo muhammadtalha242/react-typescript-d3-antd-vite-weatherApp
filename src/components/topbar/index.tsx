e3d3f6ecae19cc7a702a3294cf2ba93a81c6dae1
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-14 02:55:18 +0500

    topbar location search

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,29 +1,74 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
+import debounce from "lodash.debounce";
 
-import InputSearch from "../common/input-search";
+import InputSearch, { ISearch } from "../common/input-search";
 import { HeaderContainer } from "./container";
 import GeoCodingService from "../../services/geocodeing-api";
+import WeatherService from "../../services/weather-api";
 
 const TopBar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState({ name: "", lat: "", lon: "" });
+  const [options, setOptions] = useState<ISearch>({
+    searchTerm: "",
+    searchedOptions: [
+      { value: "", label: "", coordinates: { lat: "", lon: "" } },
+    ],
+  });
 
-  const handleInput = async ({
-    name,
-    value,
-  }: {
-    name: string;
-    value: string;
-  }) => {
-    setCity(value);
-    if (value.length > 4) {
-      const res = await GeoCodingService.getName(value);
-      console.log("res: ", res);
-    }
+  const handleInput = async ({ value }: { value: string }) => {
+    const currentState = { ...options };
+    setOptions({
+      searchTerm: value,
+      searchedOptions: currentState.searchedOptions,
+    });
+    getCityName(value);
   };
 
+  const getCityName = useCallback(
+    debounce(async (value: string) => {
+      try {
+        const res = await GeoCodingService.getName(value);
+        console.log("🚀 ~ file: index.tsx:34 ~ debounce ~ res", res);
+
+        const citiesArray: {
+          value: string;
+          label: string;
+          coordinates: { lat: string; lon: string };
+        }[] = res.map((ele: any, index: number) => {
+          return {
+            label: `${ele.name}, ${ele.state}`,
+            value: `${ele.name}, ${ele.state}`,
+            coordinates: { lat: ele.lat, lon: ele.lon },
+          };
+        });
+        setOptions({ searchTerm: value, searchedOptions: citiesArray });
+      } catch (e: any) {
+        console.log("🚀 ~ file: index.tsx:49 ~ debounce ~ e", e);
+      }
+    }, 500),
+    []
+  );
+
+  const handleSelectedValue = async (selectedOption: any) => {
+    console.log(
+      "🚀 ~ file: index.tsx:56 ~ handleSelectedValue ~ selectedOption",
+      selectedOption
+    );
+    setOptions({ ...options, searchTerm: selectedOption.value });
+    setCity({
+      name: selectedOption.value,
+      lat: selectedOption.coordinates.lat,
+      lon: selectedOption.coordinates.lon,
+    });
+
+    const res = await WeatherService.getCurrentWeather({
+      ...selectedOption.coordinates,
+    });
+    console.log("🚀 ~ file: index.tsx:70 ~ handleSelectedValue ~ res", res)
+  };
   return (
     <HeaderContainer>
       <div className="left">
@@ -36,7 +81,12 @@ const TopBar = () => {
         )}
       </div>
       <div className="right">
-        <InputSearch value={city} setValue={handleInput} name="search" />
+        <InputSearch
+          handleSelectedValue={handleSelectedValue}
+          displayOptions={options}
+          setValue={handleInput}
+          name="search"
+        />
         <AiOutlineBell />
         <AiOutlineUser />
       </div>
