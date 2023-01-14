cd5375e8e551e5b691a52ff81646f16215574c67
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 00:59:00 +0500

    weather api context

---
+++ src/context/waether-api.context.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+interface IState {
+  cityName: string;
+}
+
+export const initialState: IState = {
+  cityName: "",
+};
+
+type IAction = {
+  type: string;
+  payload?: any;
+};
+
+const weatherReducer = (state: IState, action: IAction) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
+const WeatherContxt = () => {
+  const WeatherContext = createContext<{
+    state: IState;
+    dispatch?: React.Dispatch<IAction>;
+  }>(null!);
+  const Provider = ({ children }: { children: any }) => {
+    const [state, dispatch] = useReducer(weatherReducer, initialState);
+    return (
+      <WeatherContext.Provider value={{ state, dispatch }}>
+        {children}
+      </WeatherContext.Provider>
+    );
+  };
+  return <div>WeatherContext</div>;
+};
+
+export default WeatherContxt;
