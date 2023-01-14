a598a8708036e9a3208b8027ccc97c2e0ce19351
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 01:14:15 +0500

    set city details in context

--- src/context/waether-api.context.tsx
+++ src/context/waether-api.context.tsx
@@ -1,12 +1,13 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useReducer } from "react";
 
-interface IState {
-  cityName: string;
-}
-
-export const initialState: IState = {
+const initialState = {
   cityName: "",
+  coord: { lat: "", lon: "" },
+  currentWeather: {},
+  forcastWeather: {},
 };
+
+export type IState = typeof initialState;
 
 type IAction = {
   type: string;
@@ -15,10 +16,44 @@ type IAction = {
 
 const weatherReducer = (state: IState, action: IAction) => {
   switch (action.type) {
+    case ACTION_TYPES.SET_CITY_DETAILS: {
+      return {
+        ...state,
+        cityName: action.payload.name,
+        coord: { ...action.payload.coord },
+      };
+    }
+
     default:
       return { ...state };
   }
 };
+
+const ACTION_TYPES = {
+  SET_CITY_DETAILS: "SET_CITY_DETAILS",
+  SET_CURRENT_WEATHER: "SET_CURRENT_WEATHER",
+  SET_FORCAT_WEATHER: "SET_FORCAT_WEATHER",
+};
+
+export const setCityDetails =
+  (dispatch: React.Dispatch<IAction> | undefined) =>
+  ({ name, coord }: { name: string; coord: { lat: string; lon: string } }) => {
+    if (dispatch)
+      dispatch({
+        type: ACTION_TYPES.SET_CITY_DETAILS,
+        payload: { name, coord },
+      });
+  };
+
+export const setCurrentWeather =
+  (dispatch: React.Dispatch<IAction> | undefined) => () => {
+    if (dispatch) dispatch({ type: "", payload: "" });
+  };
+
+export const setForcastWeather =
+  (dispatch: React.Dispatch<IAction> | undefined) => () => {
+    if (dispatch) dispatch({ type: "", payload: "" });
+  };
 
 const Context = () => {
   const WeatherContext = createContext<{
