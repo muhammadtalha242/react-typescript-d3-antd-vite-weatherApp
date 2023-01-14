9fdb9b8b07ff269caaa2ff7d6d7e70559df83e43
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 03:54:18 +0500

    http to https

--- src/services/geocodeing-api.ts
+++ src/services/geocodeing-api.ts
@@ -3,7 +3,7 @@ import WEATHER_API_KEY from "../constants/api";
 
 const getName = async (name: string) => {
   const res = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${WEATHER_API_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${WEATHER_API_KEY}`
   );
   return res.data;
 };
