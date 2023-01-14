f190f44bbc7fb8e2dc0d4ab3291bba5396d98aa5
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 03:14:46 +0500

    styles => colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,34 @@
+export const GREY_PRIMARY = '#E0E0E0';
+export const GREY_SECONDARY = '#828282';
+export const GREY_TERIARY = '#4F4F4F';
+export const GREY_QUATERNARY = '#F9F9F9';
+export const GREY_QUINARY = '#728CA1';
+export const GREY_SENARY = '#BDBDBD';
+export const GREY_SEPTENARY = '#F2F2F2';
+export const GREY_OCTAL = '#FAFAFA';
+
+export const BLUE_SECONDARY = '#56CCF2';
+export const BLUE_TERTIARY = '#56A7F2';
+export const BLUE_QUATERNARY = '#2D9CDB';
+
+export const GREEN_PRIMARY = '#09BC8A';
+export const GREEN_SECONDARY = '#3CE2C4';
+export const GREEN_TERTIARY = '#75DDDD';
+
+export const NAVY_PRIMARY = '#172A3A';
+export const NAVY_GRAY = '#728CA2';
+
+export const RED_PRIMARY = '#EB5757';
+
+export const ORANGE_PRIMARY = '#FFAE50';
+export const ORANGE_SECONDARY = '#FF7426';
+
+export const PINK_PRIMARY = '#FB37A0';
+
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+
+export const ROCK_BLUE = '#98ABC5';
+export const BLUE_VIOLET = '#833DF2';
+export const SCOOTER = '#4AE0E6';
+export const PURPLE_LIGHT = '#FB37A0';
