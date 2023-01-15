0e66f0e27bbad2e7d6862e05c6babdb414277e2f
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 08:40:52 +0500

    sidebar component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useState } from "react";
+import { BsHouse, BsMap, BsFillPinMapFill, BsCalendar3 } from "react-icons/bs";
+import { Layout, Menu } from "antd";
+import { redirect, useNavigate } from "react-router-dom";
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+  const [collapsed, setCollapsed] = useState(false);
+  const { Sider } = Layout;
+  const navigate = useNavigate();
+  const onItemSelect = ({
+    item,
+    key,
+    keyPath,
+    selectedKeys,
+    domEvent,
+  }: {
+    item: any;
+    key: any;
+    keyPath: any;
+    selectedKeys: any;
+    domEvent: any;
+  }) => {
+    console.log("🚀 ~ file: index.tsx:43 ~ SideBar ~ key", key);
+    return navigate(`/${key}`);
+  };
 
-export default SideBar
+  return (
+    <Sider
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+      style={{ background: "#EEF2F3" }}
+    >
+      <div className="logo"> TEST</div>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["dashboard"]}
+        style={{ background: "#EEF2F3" }}
+        onSelect={onItemSelect}
+        items={[
+          {
+            key: "dashboard",
+            label: "Dashboard",
+            icon: <BsHouse />,
+          },
+          {
+            key: "map",
+            label: "Map",
+            icon: <BsMap />,
+          },
+          {
+            key: "saved-locations",
+            label: "Saved locations",
+            icon: <BsFillPinMapFill />,
+          },
+          {
+            key: "calendar",
+            label: "Calendar",
+            icon: <BsCalendar3 />,
+          },
+        ]}
+      />
+    </Sider>
+  );
+};
+
+export default SideBar;
