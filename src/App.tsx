8c1316728b1e7ce05c72c6a141d42f961c3a7b9c
muhammadtalha242 <m.talha2244@gmail.com>
2023-01-15 09:24:17 +0500

    code clean up

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,6 @@ import TopBar from "./components/topbar";
 import SideBar from "./components/sidebar";
 
 const LayoutContainer = styled(Layout)`
-  border: 1px solid;
-  height: 100vh;
-
-  .trigger {
-    padding: 0 24px;
-    font-size: 18px;
-    line-height: 64px;
-    cursor: pointer;
-    transition: color 0.3s;
-  }
-
-  .trigger:hover {
-    color: #1890ff;
-  }
-
-  .logo {
-    height: 32px;
-    margin: 16px;
-    background: rgba(255, 255, 255, 0.3);
-  }
 `;
 
 const {Content } = Layout;
