1d1529fb896b4617de779127c1527100780aa362
muhammadtalha242 <m.talha2244@gmail.com>
2023-02-06 03:44:53 +0500

    new service

--- src/services/geocodeing-api.ts
+++ src/services/geocodeing-api.ts
@@ -1,15 +1,29 @@
 import axios from "axios";
 import WEATHER_API_KEY from "../constants/api";
 
-const getName = async (name: string) => {
+const getCities = async (name: string) => {
   const res = await axios.get(
     `https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${WEATHER_API_KEY}`
   );
   return res.data;
 };
 
+const getCityName = async ({
+  lat,
+  lon,
+}: {
+  lat: string | number;
+  lon: string | number;
+}) => {
+  const res = await axios.get(
+    `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${WEATHER_API_KEY}`
+  );
+  return res.data;
+};
+
 const exports = {
-  getName,
+  getCities,
+  getCityName,
 };
 
 export default exports;
